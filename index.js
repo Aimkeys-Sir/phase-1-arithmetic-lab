a193b4fea76cd6fa31f5ce17ea1ec9f3d9e254da
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-06-08 12:58:46 +0300

    commit tree 29339caeacdfb94e599f83c682e80476c37ea49f

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const set=[2,5,6,7,19,20,12,16]
+const num1=2;
+const num2=31
+const num4=20;
+const num3=16;
+
+let multiply=(_num1,_num2) => {return _num1*_num2};
+multiply=multiply(num1,num2);
+
+let random = function(){return Math.floor(Math.random()*10+1)}
+const remainder= (_num3,_num4) => _num4%_num3;
+const theMax= (_num3,_num4) => Math.max(_num3,_num4);
+random= random();
+let mod=remainder(num3,num4);
+let max=theMax(num3,num4);
+
+console.log(random);
 
 // Write your code here
